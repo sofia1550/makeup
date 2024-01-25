6ab49276b85b395ef9dcd1b1be7745f2b5f2ddc8
sofia1550 <151698640+sofia1550@users.noreply.github.com>
2024-01-25 12:47:33 -0300

    Update chatBot.tsx

--- src/app/components/chatBot/chatBot.tsx
+++ src/app/components/chatBot/chatBot.tsx
@@ -1,20 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import styled from "@emotion/styled";
 import Image from "next/image";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+const ToggleButton = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+`;
 
-const Container = styled.div`
+const ChatIcon = styled.button`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: #d3b8ae; // Color beige para el icono del chat
+  color: white;
+  font-size: 24px;
+  border: none;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+`;
+const Container = styled.div<ContainerProps>`
   position: fixed;
   bottom: 20px;
   right: 20px;
   width: 350px;
-  border: 1px solid #007bff;
+  border: 1px solid #d3b8ae; // Color beige suave para el borde
   border-radius: 15px;
-  background-color: #fff;
-  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  background-color: #faf3f0; // Color de fondo claro
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1); // Sombra más suave
   padding: 15px;
   box-sizing: border-box;
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  display: ${({ isExpanded }) =>
+    isExpanded ? "block" : "none"}; // Controlar la visibilidad
+`;
+
+const Header = styled.div`
+  background-color: #d3b8ae; // Color beige para la cabecera
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const MessageList = styled.ul`
@@ -45,13 +82,13 @@ const Input = styled.input`
   width: calc(100% - 120px);
   padding: 10px 15px;
   border-radius: 25px;
-  border: 2px solid #007bff;
+  border: 2px solid #ff69b4; // Color rosa para el borde
   box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1);
   transition: border 0.3s ease;
   color: black;
 
   &:focus {
-    border-color: #0056b3;
+    border-color: #ff1493; // Un rosa más oscuro para el foco
     outline: none;
   }
 `;
@@ -72,29 +109,38 @@ const Button = styled.button`
   margin-left: 10px;
   border-radius: 25px;
   border: none;
-  background-color: #007bff;
+  background-color: #ff69b4;
   color: white;
   font-weight: bold;
   cursor: pointer;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #0056b3;
+    background-color: #ff1493;
   }
 `;
 interface Message {
   author: string;
   content: string;
-  imageUrl?: string; 
+  imageUrl?: string;
 }
 interface Product {
   nombre: string;
   descripcion: string;
-  imagen_url: string; 
+  imagen_url: string;
+}
+interface ContainerProps {
+  isExpanded: boolean;
 }
 const Chatbot: React.FC = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState<Message[]>([]);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const toggleChatbot = () => {
+    setIsExpanded(!isExpanded); // Cambiar el estado para expandir o contraer el chatbot
+  };
 
   const sendMessage = async () => {
     if (!input.trim()) return;
@@ -105,7 +151,7 @@ const Chatbot: React.FC = () => {
 
     try {
       const response = await axios.post(
-        "https://asdasdasd3.onrender.com/api/products/search",
+        "http://localhost:3002/api/products/search",
         { query: input }
       );
       const responseData = response.data;
@@ -117,7 +163,7 @@ const Chatbot: React.FC = () => {
           content: `Producto encontrado: ${product.nombre} - ${product.descripcion}`,
           imageUrl: product.imagen_url.startsWith("http")
             ? product.imagen_url
-            : `https://asdasdasd3.onrender.com${product.imagen_url}`,
+            : `http://localhost:3002${product.imagen_url}`,
         };
       });
 
@@ -134,40 +180,75 @@ const Chatbot: React.FC = () => {
 
     setInput(""); // Limpiar el campo de entrada
   };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        setIsExpanded(false);
+      }
+    };
 
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [containerRef]);
   return (
-    <Container>
-      <MessageList>
-        {messages.map((message, index) => (
-          <MessageItem key={index} author={message.author}>
-            {message.content}
-            {message.imageUrl && (
-              <div style={{ marginTop: "10px" }}>
-                <Image
-                  src={message.imageUrl}
-                  alt="Product"
-                  style={{
-                    maxWidth: "100px",
-                    maxHeight: "100px",
-                    borderRadius: "10px",
-                  }}
-                />
-              </div>
-            )}
-          </MessageItem>
-        ))}
-      </MessageList>
-      <div>
-        <Input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-        />
-        <Button onClick={sendMessage}>Enviar</Button>
-      </div>
-    </Container>
+    <>
+      {isExpanded ? (
+        <Container ref={containerRef} isExpanded={isExpanded}>
+          <Header>
+            <div>Chatbot</div>
+            <ToggleButton onClick={toggleChatbot}>
+              <ExpandLessIcon />
+            </ToggleButton>
+          </Header>
+          <MessageList>
+            {messages.map((message, index) => (
+              <MessageItem key={index} author={message.author}>
+                {message.content}
+                {message.imageUrl && (
+                  <div
+                    style={{
+                      marginTop: "10px",
+                      position: "relative",
+                      width: "100px",
+                      height: "100px",
+                    }}
+                  >
+                    <img
+                      src={message.imageUrl}
+                      alt="Product"
+                      style={{
+                        width: "100%", // Controla el ancho de la imagen
+                        height: "100%", // Controla la altura de la imagen
+                        objectFit: "cover", // Asegura que la imagen cubra el contenedor
+                        borderRadius: "10px", // Aplica bordes redondeados
+                      }}
+                    />
+                  </div>
+                )}
+              </MessageItem>
+            ))}
+          </MessageList>
+          <div>
+            <Input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+            />
+            <Button onClick={sendMessage}>Enviar</Button>
+          </div>
+        </Container>
+      ) : (
+        <ChatIcon onClick={toggleChatbot}>
+          <ChatBubbleOutlineIcon />
+        </ChatIcon>
+      )}
+    </>
   );
 };
 
 export default Chatbot;
+
